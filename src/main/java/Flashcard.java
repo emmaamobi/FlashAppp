77e6181d02a7f8adc52c02f2052c30749766e563
lladewig <lladewg@luc.edu>
2018-11-18 19:32:47 -0600

    completed TestFlashcard

--- src/main/java/Flashcard.java
+++ src/main/java/Flashcard.java
@@ -4,6 +4,12 @@ public class Flashcard {
     String definition;
 
     public Flashcard(String term, String definition){
+        if (term == null){
+            throw new IllegalArgumentException("term is null");
+        }
+        if (definition == null){
+            throw new IllegalArgumentException("definition is null");
+        }
         this.term = term;
         this.definition = definition;
     }
