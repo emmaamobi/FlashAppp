1788ee9b673d721716c1ebe6d35bcda3f7a5d80f
Collin Jones <cjones28@luc.edu>
2018-11-19 02:13:52 +0000

    did cardstack

--- src/main/java/CardStack.java
+++ src/main/java/CardStack.java
@@ -1,2 +1,22 @@
+import java.util.*;
+
 public class CardStack {
+    private Stack<Flashcard> stackOCards;
+    
+    private void push(Flashcard f)// method pushes flashcard to top of stack
+    {
+        stackOCards.push(f);   
+    } 
+    
+    private Flashcard pop(){ // method returns flashcard from top of stack and removes it
+        return stackOCards.pop();
+
+    }
+    
+    private Flashcard peek(){ // method returns flashcard from top of stack but doesn't remove it
+        return stackOCards.peek();
+    }
+    
+    
+    
 }
