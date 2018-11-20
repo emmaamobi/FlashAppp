dbc258dfbfa4e10405c11280e2f3cf6c97b2eb0b
AmobiSomto <amobisomto@gmail.com>
2018-11-19 19:43:58 -0600

    added cardStack method

--- src/main/java/CardStack.java
+++ src/main/java/CardStack.java
@@ -1,2 +1,17 @@
+import java.util.Stack;
+
 public class CardStack {
+    private Stack<Flashcard> aStack;
+
+
+    public void push(Flashcard f){
+        aStack.push(f);
+    }
+    public Flashcard pop(){
+        return aStack.pop();
+    }
+    public Flashcard peek(){
+        return aStack.peek();
+    }
+
 }
