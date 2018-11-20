748fddb4113da138490c93baf2ec217b2600ba4c
Lindsay <lladewig>
2018-11-19 21:06:01 -0600

    building card stack test

--- src/main/java/CardStack.java
+++ src/main/java/CardStack.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class CardStack {
-    private Stack<Flashcard> stackOCards;
+    public Stack<Flashcard> stackOCards;
     
-    private void push(Flashcard f)// method pushes flashcard to top of stack
+    public void push(Flashcard f)// method pushes flashcard to top of stack
     {
         stackOCards.push(f);   
     } 
     
-    private Flashcard pop(){ // method returns flashcard from top of stack and removes it
+    public Flashcard pop(){ // method returns flashcard from top of stack and removes it
         return stackOCards.pop();
 
     }
     
-    private Flashcard peek(){ // method returns flashcard from top of stack but doesn't remove itcd 
+    public Flashcard peek(){ // method returns flashcard from top of stack but doesn't remove itcd
         return stackOCards.peek();
     }
     
--- src/test/java/TestStack.java
+++ src/test/java/TestStack.java
@@ -1,2 +1,46 @@
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+
 public class TestStack {
+
+    private CardStack fixture;
+
+    @Before
+    public void setUp(){
+        fixture = new CardStack();
+    }
+
+    @After
+    public void tearDown(){
+        fixture = null;
+    }
+
+    @Test
+    public void testPush(){
+        Flashcard f = new Flashcard("carbon", "6 protons");
+        fixture.push(f);
+        assertEquals(f, fixture.peek());
+    }
+
+    @Test
+    public void testPop(){
+        Flashcard f = new Flashcard("carbon", "6 protons");
+        fixture.push(f);
+        assertEquals(f, fixture.peek());
+        assertEquals(f, fixture.pop());
+    }
+
+    @Test
+    public void testPeek(){
+
+    }
+
+
+
+
+
+
 }
